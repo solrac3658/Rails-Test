b0e121162b986d36c9e2010cb65bfd5d1e05103a
Carlos Sanguña <carlos.daniel.009@hotmail.com>
2020-02-20 16:24:32 -0300

    Creacion de Peliculas y Series

--- app/controllers/arrendar_controller.rb
+++ app/controllers/arrendar_controller.rb
@@ -1,4 +1,13 @@
 class ArrendarController < ApplicationController
+  
+  def createFormPelicula 
+  	@peliculas = [["Rapidos y_furiosos",1]]
+  end
+
+  def createFormSerie
+  	@series = [["naruto", 2]]
+  end
+
   def arrendo
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+	# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+peliculas = Entretenimiento.create([
+	{ titulo: 'Star Wars', genero: "Ciencia Ficcion", precio:3000, tipo:"Pelicula", duracion: 2.35}, 
+	{ titulo: 'Lord of the Rings' , genero: "Ciencia Ficcion", precio:3000, tipo:"Pelicula", duracion: 2.15},
+	{ titulo: 'Batman Dark Night' , genero: "Ciencia Ficcion", precio:3000, tipo:"Pelicula", duracion: 2.00},
+	{ titulo: 'Prison Breack' , genero: "Ciencia Ficcion", precio:7000, tipo:"Serie", temporada:4},
+	{ titulo: 'Sherlock' , genero: "Ciencia Ficcion", precio:7000, tipo:"Serie", temporada:4},
+	{ titulo: 'Dr House' , genero: "Ciencia", precio:9000, tipo:"Serie", temporada:8},
+])
+
